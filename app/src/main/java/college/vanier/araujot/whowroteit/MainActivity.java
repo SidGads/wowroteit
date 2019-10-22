adbdfb777b720671ec31cb895333a11af46c1b97
SidGads <SidTheKid42>
2019-10-22 13:07:43 -0400

    trying to figure something out

--- app/src/main/java/college/vanier/araujot/whowroteit/MainActivity.java
+++ app/src/main/java/college/vanier/araujot/whowroteit/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.loader.content.Loader;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,10 +18,24 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
 
@@ -28,6 +43,19 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     private TextView mTitleText;
     private TextView mAuthorText;
     private ImageView mImageView;
+    private TextView textView ;
+
+    private static final String LOG_TAG =
+            NetworkUtils.class.getSimpleName();
+
+    // Base URL for Books API.
+    private static final String BOOK_BASE_URL =  "https://www.googleapis.com/books/v1/volumes?";
+    // Parameter for the search string.
+    private static final String QUERY_PARAM = "q";
+    // Parameter that limits search results.
+    private static final String MAX_RESULTS = "maxResults";
+    // Parameter to filter by print type.
+    private static final String PRINT_TYPE = "printType";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +66,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         mTitleText = findViewById(R.id.titleText);
         mAuthorText = findViewById(R.id.authorText);
         mImageView = findViewById(R.id.imageView);
+        textView =(TextView) findViewById(R.id.instructions);
     }
 
     public void searchBooks(View view) {
@@ -145,4 +174,103 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onLoaderReset(@NonNull Loader<String> loader) {
     }
+
+    public void volleyAttemp(View view) {
+
+        String url = "https://www.googleapis.com/books/v1/volumes?";
+
+        EditText et = (EditText) findViewById(R.id.bookInput);
+        String reqStr = et.toString();
+
+        try {
+            Uri builtURI = Uri.parse(BOOK_BASE_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARAM, reqStr)
+                    .appendQueryParameter(MAX_RESULTS, "10")
+                    .appendQueryParameter(PRINT_TYPE, "books")
+                    .build();
+
+
+            RequestQueue queue = Volley.newRequestQueue(this);
+            Uri uri = builtURI.parse(BOOK_BASE_URL);
+
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                iterpretJSONData(response);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    textView.setText("That didn't work!");
+                }
+            });
+
+// Add the request to the RequestQueue.
+            queue.add(stringRequest);
+
+
+        } catch (Exception e) {
+        }
+    }
+
+
+
+
+// Request a string response from the provided URL.
+
+
+
+        public void iterpretJSONData(String JSONstr) throws JSONException {
+            JSONObject jsobj = new JSONObject(JSONstr);
+
+            try {
+                JSONObject jsonObject = new JSONObject(JSONstr);
+                JSONArray itemsArray = jsonObject.getJSONArray("items");
+                int i = 0;
+                String title = null;
+                String authors = null;
+
+                while (i < itemsArray.length() &&
+                        (authors == null && title == null)) {
+                    // Get the current item information.
+                    JSONObject book = itemsArray.getJSONObject(i);
+                    JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                    // Try to get the author and title from the current item,
+                    // catch if either field is empty and move on.
+                    try {
+                        title = volumeInfo.getString("title");
+                        authors = volumeInfo.getString("authors");
+
+                        if (title != null && authors != null) {
+                            mTitleText.setText(title);
+                            mAuthorText.setText(authors);
+                        } else {
+                            mTitleText.setText(R.string.no_results);
+                            mAuthorText.setText("");
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    // Move to the next item.
+                    i++;
+                }
+            } catch (Exception e) {
+                // If onPostExecute does not receive a proper JSON string,
+                // update the UI to show failed results.
+                mTitleText.setText(R.string.no_results);
+                mAuthorText.setText("");
+            }
+        }
+
+
 }
